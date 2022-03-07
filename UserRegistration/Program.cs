e348023433afb438c12c72275152d00303ba9514
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-07 11:08:56 +0530

    UserRegg

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using UserRegistration;
+
+Console.Title = "User Registration";
+Console.WriteLine("==========User Registration==========");
+
+Console.WriteLine("Using Reflection to enter Registration form for a new user");
+try
+{
+    Type type = typeof(Registration);
+    var form = Activator.CreateInstance(type);
+    MethodInfo methodInfo = type.GetMethod("GetInfo");
+    methodInfo.Invoke(form, null);
+}
+catch
+{
+    Console.WriteLine("Reflection failed");
+}
---
+++ UserRegistration/Registration.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistration;
+
+/// <summary>
+/// Takes details from user for registration form purposes
+/// </summary>
+public class Registration
+{
+    // RegEx patterns for various types of information
+    public const string NAME_PATTERN = @"^[A-Z][a-zA-Z]{2,}$";
+    public const string EMAIL_PATTERN = @"^[A-Za-z0-9]{3,}([\.\-\+][A-Za-z0-9]{3,})?[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2,})?$";
+    public const string MOBILE_PATTERN = @"^[0-9]{2}[ ][0-9]{10}$";
+    public const string PASSWORD_PATTERN = @"^(?!.*[!@#&()–\[{}\]:;',?/*~$^+=<>].*[!@#&()–\[{}\]:;',?/*~$^+=<>])(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+
+    // Registration details of user
+    private string firstName;
+    private string lastName;
+    private string email;
+    private string mobile;
+    private string password;
+
+    /// <summary>
+    /// Gets the information from user.
+    /// </summary>
+    public void GetInfo()
+    {
+        firstName = GetValidInfo("First Name: ", NAME_PATTERN);
+        lastName = GetValidInfo("Last Name: ", NAME_PATTERN);
+        email = GetValidInfo("Email: ", EMAIL_PATTERN);
+        mobile = GetValidInfo("Mobile: ", MOBILE_PATTERN);
+        password = GetValidInfo("Password: ", PASSWORD_PATTERN);
+    }
+
+    /// <summary>
+    /// Gets the valid information from user.
+    /// <para>based on the pattern, this ensures the user enters valid info pattern</para>
+    /// </summary>
+    /// <returns>A valid info string</returns>
+    private static string GetValidInfo(string message, string pattern)
+    {
+        string info;
+        try
+        {
+            do
+            {
+                Console.Write(message);
+                info = Console.ReadLine();
+                if (IsValid(info, pattern))
+                    return info;
+                else
+                    Console.WriteLine("Invalid!");
+            } while (true);
+        }
+        catch (NullReferenceException)
+        {
+            Console.WriteLine("Cannot give empty or null information");
+        }
+        catch
+        {
+            Console.WriteLine("Unkown error occured");
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Returns true if the info is valid i.e matches the pattern.
+    /// </summary>
+    /// <returns>
+    ///   <c>true</c> if the specified name is valid; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool IsValid(string info, string pattern)
+    {
+        return Regex.IsMatch(info, pattern);
+    }
+}
