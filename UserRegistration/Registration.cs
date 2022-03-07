59fa903b65e574d5933ca69e75e50cec49dafe86
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-07 17:57:24 +0530

    ChecksEmail

--- UserRegistration/Registration.cs
+++ UserRegistration/Registration.cs
@@ -7,32 +7,52 @@ namespace UserRegistration;
 /// </summary>
 internal class Registration
 {
+    // RegEx patterns for various types of information
+    private const string namePattern = @"^[A-Z][a-zA-Z]{2,}$";
+    private const string emailPattern = @"^[A-Za-z0-9]{3,}([.][A-Za-z0-9]{3,})?[@][a-zA-Z]{2,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2})?$";
+
+    // Registration details of user
     private string firstName;
     private string lastName;
+    private string email;
 
+    /// <summary>
+    /// Gets the information from user.
+    /// </summary>
     public void GetInfo()
     {
-        firstName = GetValidName("First Name: ");
-        lastName = GetValidName("Last Name: ");
+        firstName = GetValidInfo("First Name: ", namePattern);
+        lastName = GetValidInfo("Last Name: ", namePattern);
+        email = GetValidInfo("Email: ", emailPattern);
     }
 
-    private string GetValidName(string message)
+    /// <summary>
+    /// Gets the valid information from user.
+    /// <para>based on the pattern, this ensures the user enters valid info pattern</para>
+    /// </summary>
+    /// <returns>A valid info string</returns>
+    private static string GetValidInfo(string message, string pattern)
     {
-        string name;
+        string info;
         do
         {
             Console.Write(message);
-            name = Console.ReadLine();
-            if (IsValidName(name))
-                return name;
+            info = Console.ReadLine();
+            if (IsValid(info, pattern))
+                return info;
             else
                 Console.WriteLine("Invalid! (First letter caps, Min 3 characters)");
         } while (true);
     }
 
-    private bool IsValidName(string name)
+    /// <summary>
+    /// Returns true if the info is valid i.e matches the pattern.
+    /// </summary>
+    /// <returns>
+    ///   <c>true</c> if the specified name is valid; otherwise, <c>false</c>.
+    /// </returns>
+    private static bool IsValid(string info, string pattern)
     {
-        string pattern = @"^[A-Z][a-zA-Z]{2,}$";
-        return Regex.IsMatch(name, pattern);
+        return Regex.IsMatch(info, pattern);
     }
 }
