6846270e19e26d93cd35d599472208b33bf290f0
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-07 11:53:09 +0530

    ChecksFirstName

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,18 +1,8 @@
-﻿using System.Reflection;
-using UserRegistration;
+﻿using UserRegistration;
 
 Console.Title = "User Registration";
 Console.WriteLine("==========User Registration==========");
 
-Console.WriteLine("Using Reflection to enter Registration form for a new user");
-try
-{
-    Type type = typeof(Registration);
-    var form = Activator.CreateInstance(type);
-    MethodInfo methodInfo = type.GetMethod("GetInfo");
-    methodInfo.Invoke(form, null);
-}
-catch
-{
-    Console.WriteLine("Reflection failed");
-}
+Registration form = new Registration();
+
+form.GetInfo();
--- UserRegistration/Registration.cs
+++ UserRegistration/Registration.cs
@@ -5,72 +5,32 @@ namespace UserRegistration;
 /// <summary>
 /// Takes details from user for registration form purposes
 /// </summary>
-public class Registration
+internal class Registration
 {
-    // RegEx patterns for various types of information
-    public const string NAME_PATTERN = @"^[A-Z][a-zA-Z]{2,}$";
-    public const string EMAIL_PATTERN = @"^[A-Za-z0-9]{3,}([\.\-\+][A-Za-z0-9]{3,})?[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2,})?$";
-    public const string MOBILE_PATTERN = @"^[0-9]{2}[ ][0-9]{10}$";
-    public const string PASSWORD_PATTERN = @"^(?!.*[!@#&()–\[{}\]:;',?/*~$^+=<>].*[!@#&()–\[{}\]:;',?/*~$^+=<>])(?=.*[A-Z])(?=.*[0-9]).{8,}$";
-
-    // Registration details of user
     private string firstName;
-    private string lastName;
-    private string email;
-    private string mobile;
-    private string password;
 
-    /// <summary>
-    /// Gets the information from user.
-    /// </summary>
     public void GetInfo()
     {
-        firstName = GetValidInfo("First Name: ", NAME_PATTERN);
-        lastName = GetValidInfo("Last Name: ", NAME_PATTERN);
-        email = GetValidInfo("Email: ", EMAIL_PATTERN);
-        mobile = GetValidInfo("Mobile: ", MOBILE_PATTERN);
-        password = GetValidInfo("Password: ", PASSWORD_PATTERN);
+        firstName = GetValidName("First Name: ");
     }
 
-    /// <summary>
-    /// Gets the valid information from user.
-    /// <para>based on the pattern, this ensures the user enters valid info pattern</para>
-    /// </summary>
-    /// <returns>A valid info string</returns>
-    private static string GetValidInfo(string message, string pattern)
+    private string GetValidName(string message)
     {
-        string info;
-        try
+        string name;
+        do
         {
-            do
-            {
-                Console.Write(message);
-                info = Console.ReadLine();
-                if (IsValid(info, pattern))
-                    return info;
-                else
-                    Console.WriteLine("Invalid!");
-            } while (true);
-        }
-        catch (NullReferenceException)
-        {
-            Console.WriteLine("Cannot give empty or null information");
-        }
-        catch
-        {
-            Console.WriteLine("Unkown error occured");
-        }
-        return null;
+            Console.Write(message);
+            name = Console.ReadLine();
+            if (IsValidName(name))
+                return name;
+            else
+                Console.WriteLine("Invalid! (First letter caps, Min 3 characters)");
+        } while (true);
     }
 
-    /// <summary>
-    /// Returns true if the info is valid i.e matches the pattern.
-    /// </summary>
-    /// <returns>
-    ///   <c>true</c> if the specified name is valid; otherwise, <c>false</c>.
-    /// </returns>
-    public static bool IsValid(string info, string pattern)
+    private bool IsValidName(string name)
     {
-        return Regex.IsMatch(info, pattern);
+        string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+        return Regex.IsMatch(name, pattern);
     }
 }
