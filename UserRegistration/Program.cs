19060ba49736815f7c3c2835a7872a4ea4183f63
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-09 17:34:56 +0530

    UsesReflection

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,9 +1,18 @@
-﻿using UserRegistration;
+﻿using System.Reflection;
+using UserRegistration;
 
 Console.Title = "User Registration";
 Console.WriteLine("==========User Registration==========");
 
-Registration form = new Registration();
-
-form.GetInfo();
-Console.ReadLine();
+Console.WriteLine("Using Reflection to enter Registration form for a new user");
+try
+{
+    Type type = typeof(Registration);
+    var form = Activator.CreateInstance(type);
+    MethodInfo methodInfo = type.GetMethod("GetInfo");
+    methodInfo.Invoke(form, null);
+}
+catch
+{
+    Console.WriteLine("Reflection failed");
+}
