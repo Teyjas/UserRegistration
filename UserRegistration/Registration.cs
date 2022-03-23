d535d9eb46a13cae55c8b97e3fa953339fef3817
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-23 16:05:35 +0530

    [Teyjas]Uses_LambdaExpression

--- UserRegistration/Registration.cs
+++ UserRegistration/Registration.cs
@@ -7,6 +7,9 @@ namespace UserRegistration;
 /// </summary>
 public class Registration
 {
+    public delegate bool ValidityCheck(string input);
+    public ValidityCheck patternMatch;
+
     // RegEx patterns for various types of information
     public const string NAME_PATTERN = @"^[A-Z][a-zA-Z]{2,}$";
     public const string EMAIL_PATTERN = @"^[A-Za-z0-9]{3,}([\.\-\+][A-Za-z0-9]{3,})?[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2,})?$";
@@ -14,11 +17,20 @@ public class Registration
     public const string PASSWORD_PATTERN = @"^(?!.*[!@#&()–\[{}\]:;',?/*~$^+=<>].*[!@#&()–\[{}\]:;',?/*~$^+=<>])(?=.*[A-Z])(?=.*[0-9]).{8,}$";
 
     // Registration details of user
-    private string firstName;
-    private string lastName;
-    private string email;
-    private string mobile;
-    private string password;
+    private string? firstName;
+    private string? lastName;
+    private string? email;
+    private string? mobile;
+    private string? password;
+
+    public Registration()
+    {
+        firstName = "";
+        lastName = "";
+        email = "";
+        mobile = "";
+        password = "";
+    }
 
     /// <summary>
     /// Gets the information from user.
@@ -27,11 +39,20 @@ public class Registration
     {
         try
         {
-            firstName = GetValidInfo("First Name: ", NAME_PATTERN, UserRegistrationException.ExceptionType.INVALID_NAME);
-            lastName = GetValidInfo("Last Name: ", NAME_PATTERN, UserRegistrationException.ExceptionType.INVALID_NAME);
-            email = GetValidInfo("Email: ", EMAIL_PATTERN, UserRegistrationException.ExceptionType.INVALID_EMAIL);
-            mobile = GetValidInfo("Mobile: ", MOBILE_PATTERN, UserRegistrationException.ExceptionType.INVALID_MOBILE);
-            password = GetValidInfo("Password: ", PASSWORD_PATTERN, UserRegistrationException.ExceptionType.INVALID_PASSWORD);
+            patternMatch = new((userInput) => { return Regex.IsMatch(userInput, NAME_PATTERN); });
+            Console.Write("First Name: ");
+            firstName = GetValidInfo(patternMatch, UserRegistrationException.ExceptionType.INVALID_NAME);
+            Console.Write("Last Name: ");
+            lastName = GetValidInfo(patternMatch, UserRegistrationException.ExceptionType.INVALID_NAME);
+            patternMatch = new((userInput) => { return Regex.IsMatch(userInput, EMAIL_PATTERN); });
+            Console.Write("Email: ");
+            email = GetValidInfo(patternMatch, UserRegistrationException.ExceptionType.INVALID_EMAIL);
+            patternMatch = new((userInput) => { return Regex.IsMatch(userInput, MOBILE_PATTERN); });
+            Console.Write("Mobile: ");
+            mobile = GetValidInfo(patternMatch, UserRegistrationException.ExceptionType.INVALID_MOBILE);
+            patternMatch = new((userInput) => { return Regex.IsMatch(userInput, PASSWORD_PATTERN); });
+            Console.Write("Password: ");
+            password = GetValidInfo(patternMatch, UserRegistrationException.ExceptionType.INVALID_PASSWORD);
         }
         catch (UserRegistrationException e)
         {
@@ -65,14 +86,12 @@ public class Registration
     /// <para>based on the pattern, this ensures the user enters valid info pattern</para>
     /// </summary>
     /// <returns>A valid info string</returns>
-    private static string GetValidInfo(string message, string pattern, UserRegistrationException.ExceptionType type)
+    private static string GetValidInfo(ValidityCheck Match, UserRegistrationException.ExceptionType type)
     {
-        string info;
         try
         {
-            Console.Write(message);
-            info = Console.ReadLine();
-            if (IsValid(info, pattern))
+            string info = Console.ReadLine();
+            if (Match(info))
                 return info;
             else
                 throw new UserRegistrationException(type, "Does not match pattern");
