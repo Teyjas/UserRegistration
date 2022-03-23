6e40e638a6803c44bb645716fb7d077a3293aeb3
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-23 15:52:30 +0530

    [Teyjas]Uses_ExceptionCase

--- UserRegistration/Registration.cs
+++ UserRegistration/Registration.cs
@@ -25,11 +25,39 @@ public class Registration
     /// </summary>
     public void GetInfo()
     {
-        firstName = GetValidInfo("First Name: ", NAME_PATTERN);
-        lastName = GetValidInfo("Last Name: ", NAME_PATTERN);
-        email = GetValidInfo("Email: ", EMAIL_PATTERN);
-        mobile = GetValidInfo("Mobile: ", MOBILE_PATTERN);
-        password = GetValidInfo("Password: ", PASSWORD_PATTERN);
+        try
+        {
+            firstName = GetValidInfo("First Name: ", NAME_PATTERN, UserRegistrationException.ExceptionType.INVALID_NAME);
+            lastName = GetValidInfo("Last Name: ", NAME_PATTERN, UserRegistrationException.ExceptionType.INVALID_NAME);
+            email = GetValidInfo("Email: ", EMAIL_PATTERN, UserRegistrationException.ExceptionType.INVALID_EMAIL);
+            mobile = GetValidInfo("Mobile: ", MOBILE_PATTERN, UserRegistrationException.ExceptionType.INVALID_MOBILE);
+            password = GetValidInfo("Password: ", PASSWORD_PATTERN, UserRegistrationException.ExceptionType.INVALID_PASSWORD);
+        }
+        catch (UserRegistrationException e)
+        {
+            switch (e.type)
+            {
+                case UserRegistrationException.ExceptionType.INVALID_NAME:
+                    Console.WriteLine("Invalid Name! Must start with capital and min 3 letters");
+                    break;
+                case UserRegistrationException.ExceptionType.INVALID_EMAIL:
+                    Console.WriteLine("Inavlid Email!");
+                    break;
+                case UserRegistrationException.ExceptionType.INVALID_MOBILE:
+                    Console.WriteLine("Invalid Mobile! Must include country code, empty space and 10 numbers after that");
+                    break;
+                case UserRegistrationException.ExceptionType.INVALID_PASSWORD:
+                    Console.WriteLine("Invalid Password! following rules apply:");
+                    Console.WriteLine("1. Min 8 characters");
+                    Console.WriteLine("2. At least 1 Uppercase");
+                    Console.WriteLine("3. At least 1 number");
+                    Console.WriteLine("4. Exactly 1 special character");
+                    break;
+                default:
+                    Console.WriteLine(e.Message);
+                    break;
+            }
+        }
     }
 
     /// <summary>
@@ -37,28 +65,21 @@ public class Registration
     /// <para>based on the pattern, this ensures the user enters valid info pattern</para>
     /// </summary>
     /// <returns>A valid info string</returns>
-    private static string GetValidInfo(string message, string pattern)
+    private static string GetValidInfo(string message, string pattern, UserRegistrationException.ExceptionType type)
     {
         string info;
         try
         {
-            do
-            {
-                Console.Write(message);
-                info = Console.ReadLine();
-                if (IsValid(info, pattern))
-                    return info;
-                else
-                    Console.WriteLine("Invalid!");
-            } while (true);
+            Console.Write(message);
+            info = Console.ReadLine();
+            if (IsValid(info, pattern))
+                return info;
+            else
+                throw new UserRegistrationException(type, "Does not match pattern");
         }
         catch (NullReferenceException)
         {
             Console.WriteLine("Cannot give empty or null information");
-        }
-        catch
-        {
-            Console.WriteLine("Unkown error occured");
         }
         return null;
     }
---
+++ UserRegistration/UserRegistration_Exception.cs
@@ -0,0 +1,29 @@
+﻿namespace UserRegistration;
+
+/// <summary>
+/// Custom Exception class for handling invalid entries in user registration
+/// </summary>
+/// <seealso cref="System.Exception" />
+public class UserRegistrationException : Exception
+{
+    public readonly ExceptionType type;
+
+    public enum ExceptionType
+    {
+        INVALID_NAME,
+        INVALID_EMAIL,
+        INVALID_MOBILE,
+        INVALID_PASSWORD,
+        INVALID_ENTRY
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserRegistrationException"/> class.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <param name="errorMessage">The error message.</param>
+    public UserRegistrationException(ExceptionType type, string errorMessage) : base(errorMessage)
+    {
+        this.type = type;
+    }
+}
